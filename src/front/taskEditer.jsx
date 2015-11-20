937ffb3d8253b92d7f195cdeb6681c4cac5e85d6
yamatatsu <the.kinnikuman@gmail.com>
2015-11-21 01:03:01 +0900

    TODOリストを書くそぶりを見せる

---
+++ src/front/taskEditer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TaskEditer extends React.Component {
+  render() {
+    return <h1>TaskEditer</h1>
+  }
+}
+
+React.render(<TaskEditer/>, document.getElementById('task-editer'));
