cd74b3838e7a2b0857a9291ffa2fd830139441ed
yamatatsu <the.kinnikuman@gmail.com>
2016-04-27 17:00:17 +0900

    ナウいreactに書き換え

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -1,46 +1,41 @@
 import React from 'react';
+import {render} from 'react-dom';
 import {Router, Route, Link, browserHistory} from 'react-router';
 
 import {} from 'lodash';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <ul>
-          <li><Link to="/person" >個人</Link></li>
-          <li><Link to="/group" >グループ</Link></li>
-        </ul>
-      </div>
-    );
-  }
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        <li><Link to="/person" >個人</Link></li>
+        <li><Link to="/group" >グループ</Link></li>
+      </ul>
+    </div>
+  );
 }
 
-class Person extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>個人</h2>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-        </ul>
-      </div>
-    );
-  }
+const Person = () => {
+  return (
+    <div>
+      <h2>個人</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+      </ul>
+    </div>
+  );
 }
 
-class Group extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>グループ</h2>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-        </ul>
-      </div>
-    );
-  }
+const Group = () => {
+  return (
+    <div>
+      <h2>グループ</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+      </ul>
+    </div>
+  );
 }
 
 let router = (
@@ -51,4 +46,4 @@ let router = (
   </Router>
 );
 
-React.render(router, document.getElementById('react-root'));
+render(router, document.getElementById('react-root'));
