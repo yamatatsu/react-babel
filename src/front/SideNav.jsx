f67140095380bc542c0528435e3f054de00d6f34
yamatatsu <the.kinnikuman@gmail.com>
2016-04-28 16:39:56 +0900

    PropTypesを使って見る

--- src/front/SideNav.jsx
+++ src/front/SideNav.jsx
@@ -5,11 +5,19 @@ import {Row, Col} from 'react-bootstrap';
 let SideNav = ({sm, md}) => {
   return (
     <Col sm={sm} md={md}>
+      <Row><Link to="home" >Home</Link></Row>
       <Row><Link to="person" >個人</Link></Row>
       <Row><Link to="group" >グループ</Link></Row>
-      <Row><Link to="group" >って感じで、この辺がサイドナビ</Link></Row>
-      <Row><Link to="person" >見た目の話は別で</Link></Row>
+      <Row><Link to="person" >って感じで、この辺がサイドナビ</Link></Row>
+      <Row><Link to="group" >見た目の話は別で</Link></Row>
     </Col>
   );
 };
+
+let propTypes = React.PropTypes;
+SideNav.propTypes = {
+  sm: propTypes.number.isRequired,
+  md: propTypes.number.isRequired
+};
+
 export default SideNav;
