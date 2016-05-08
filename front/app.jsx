f587a01d0e182eb3f54c834f05830a0904829eff
yamatatsu <the.kinnikuman@gmail.com>
2016-05-08 21:26:15 +0900

    redux開始

--- front/app.jsx
+++ front/app.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
+import { render } from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistory, routeReducer } from 'redux-simple-router'
 
-import Base from 'common/Base.jsx';
-import Home from 'Home.jsx';
-import Person from 'Person.jsx';
-import Group from 'Group.jsx';
+import _ from 'lodash';
 
-/**
- * [react-routerなrouter]
- */
-const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Base}>
-      <Route path="home" component={Home} />
-      <Route path="person" component={Person} />
-      <Route path="group" component={Group} />
-    </Route>
-  </Router>
+import Base from './components/common/Base.jsx';
+import Home from './components/Home.jsx';
+import Person from './components/Person.jsx';
+import Group from './components/Group.jsx';
+
+const reducer = combineReducers(_.assign({}, { routing: routeReducer }));
+
+// Sync dispatched route actions to the history
+const reduxRouterMiddleware = syncHistory(browserHistory)
+const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware)(createStore)
+
+const store = createStoreWithMiddleware(reducer)
+
+reduxRouterMiddleware.listenForReplays(store)
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Base}>
+        <IndexRoute component={Home} />
+        <Route path="home" component={Home} />
+        <Route path="person" component={Person} />
+        <Route path="group" component={Group} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('react-root')
 );
-
-render(router, document.getElementById('react-root'));
